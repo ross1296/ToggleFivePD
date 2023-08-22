1639958f46d6e802a97eba83a204efaa1a3a59b1
Ross <mistercreic@gmail.com>
2023-08-22 01:37:31 +0100

    Add project files.

---
+++ ToggleFivePD/Main.cs
@@ -0,0 +1,24 @@
+﻿using FivePD.API;
+using System;
+
+namespace ToggleFivePD
+{
+    public class Main : Plugin
+    {
+        internal Main()
+        {
+            EventHandlers.Add("fivepd::toggleMod", new Action<bool>((toggle) => ToggleMod(toggle)));
+        }
+
+        private void ToggleMod(bool toggle)
+        {
+            if (toggle)
+            {
+                Utilities.EnableMod();
+            } else
+            {
+                Utilities.DisableMod();
+            }
+        }
+    }
+}
